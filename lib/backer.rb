44e5ab4dd3fc1685804605f73958cc1e0f53a473
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-12 20:40:38 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,4 +10,12 @@ class Backer
     ProjectBacker.new(project, self)
   end
   
+  def project_backer_instances
+    ProjectBacker.all.select{|project_backer_instance| project_backer_instance.backer == self}
+  end
+  
+  def backed_projects
+    project_backer_instances.map{|project_backer_instance| project_backer_instance.project}
+  end
+  
 end
